93e3bfd5673599473879e6785e49e214e4bc003e
fernancumez <fernancumez@gmail.com>
2020-11-20 13:01:50 -0600

    Update server/src/database.js

--- server/src/database.js
+++ server/src/database.js
@@ -1,24 +1,25 @@
-
 /*
   ?En este archivo unicamente inicamos el servidor
   ?y lo conectamos a una base de datos
 */
 
-const mongoose = require('mongoose');   //modulo para conectarnos a mongodb
+const mongoose = require("mongoose"); //modulo para conectarnos a mongodb
 
-const URI = process.env.MONGOOSE_URI  //?direccion de nuestra base datos
-  ? process.env.MONGOOSE_URI  //Condicion si no encuantra la direccion
-  : 'mongodb://localhost/notasenlinea';   //Caso contrario conectarse a esta base de datos 
+const URI = process.env.MONGODB_URI //?direccion de nuestra base datos
+  ? process.env.MONGODB_URI //Condicion si no encuantra la direccion
+  : "mongodb://localhost/notasenlinea"; //Caso contrario conectarse a esta base de datos
 
-mongoose.connect(URI, {//!Conectarnos a un sevidor de mongodb
-  useNewUrlParser: true,    //
-  useCreateIndex: true,     //     Modulo para conectar moongose
+mongoose.connect(URI, {
+  //!Conectarnos a un sevidor de mongodb
+  useNewUrlParser: true, //
+  useCreateIndex: true, //     Modulo para conectar moongose
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 const connection = mongoose.connection;
 
-connection.once('open', () => {//?Cunado la conexion sea abierta ejecutar algo por consola
-  console.log('Base de datos conectado!');
+connection.once("open", () => {
+  //?Cunado la conexion sea abierta ejecutar algo por consola
+  console.log("Base de datos conectado!");
 });
