68cc406abc2fb2645efcdac72a289ded3bd8ce11
fernancumez <fernancumez@gmail.com>
2021-01-22 18:59:43 -0600

    added page not found (404)

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import CreateNote from "./components/CreateNote";
 import CreateUser from "./components/CreateUser";
 import NotesList from "./components/NotesList";
 import About from "./components/About";
+import NotFound from "./views/404";
 
 import NoteState from "./context/notes/noteState";
 import UserState from "./context/users/userState";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import "./App.css";
+import "./styles/App.css";
 
 const App = () => (
   <NoteState>
     <UserState>
-      <Router>
+      <BrowserRouter>
         <Navigation />
         <div className="container p-4">
-          <Route path="/" exact component={NotesList} />
-          <Route path="/edit/:id" component={CreateNote} />
-          <Route path="/create" component={CreateNote} />
-          <Route path="/user" component={CreateUser} />
-          <Route path="/about" component={About} />
+          <Switch>
+            <Route path="/" exact component={NotesList} />
+            <Route path="/edit/:id" exact component={CreateNote} />
+            <Route path="/create" exact component={CreateNote} />
+            <Route path="/user" exact component={CreateUser} />
+            <Route path="/about" exact component={About} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
-      </Router>
+      </BrowserRouter>
     </UserState>
   </NoteState>
 );
