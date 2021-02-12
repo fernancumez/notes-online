957e4e03bdc053a69fe5b88a95def183257299b5
fernancumez <fernancumez@gmail.com>
2021-02-11 21:12:58 -0600

    added morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,19 @@
 import express from "express";
+import config from "./config";
+import morgan from "morgan";
 import cors from "cors";
 
-import config from "./config";
 import noteRoutes from "./routes/notes.routes";
 import userRoutes from "./routes/users.routes";
 
 const app = express();
 
-// Config
+// Configurations
 app.set("port", config.PORT);
 
 // Middlewares
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- server/src/models/user.models.js
+++ server/src/models/user.models.js
@@ -1,18 +1,17 @@
-
-//TODO: Modelo para definir que campo queremos guardar un dato
-const { Schema, model } = require('mongoose');  //?Schema => Como queremos que sean los datos(tipo de datos)    
-//?model => Como mongodb va a consultar, actualizar los datos
+import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
     username: {
-      type: String,   //El tipo de dato 
-      required: true, //El llenado de dos en este campo es obligatorio
-      unique: true,   //Evita tener espacios innecesarios
-      trim: true      //Verifica que los usuarios no se repitan
-    }
-  }, {
-  timestamps: true  //!Cuando se cree un dato, se guarde de forma automatica la fecha de creacion, actualizacion
-});
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = model('User', userSchema);
+export default model("User", userSchema);
