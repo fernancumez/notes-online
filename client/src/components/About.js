c6df762571a301088b3ef7fd50bec9b377ee48ca
fernancumez <fernancumez@gmail.com>
2021-02-27 23:09:17 -0600

    components updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,18 +1,9 @@
-//TODO: Componente about
-
 import React from "react";
 
 const About = () => (
   <div className="container">
     <h1>Acerca de</h1>
-    <hr />
-    <div className="btn btn-light text-left">
-      <h5>Este es el sistema de notas que maneja la empresa Notas en Línea</h5>
-      <h5>
-        la cual se encarga de que las personas puedan crear un usuario propio
-      </h5>
-      <h5>y así poder anotar cualquier acontecimimeto importante.</h5>
-    </div>
+    <h5>Sistema de notas</h5>
   </div>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-//TODO: Archivo principal que arranca toda nuestra aplicación
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
+import App from "./App";
 
-import React, { StrictMode } from "react"; //?importamos react
-import ReactDOM from "react-dom"; //?importamos react-dom
-import App from "./App"; //?importamos la funcion app del componente App
+const container = document.getElementById("root");
 
-ReactDOM.render(
+render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root")
-); //!Despliega la aplicacion
+  container
+);
